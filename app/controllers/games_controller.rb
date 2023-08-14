96c984c1bbcf43c32da4b1de7990a54b70d7052e
Victor Loginov <tkoe88@zoho.com>
2023-08-14 22:15:21 +0200

    English word checker added

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,13 +1,23 @@
+# frozen_string_literal: true
+
+require 'open-uri'
+# This class handles the validation of user input
 class GamesController < ApplicationController
   def new
-    @letters = ('A'..'Z').to_a.sample(10)
+    @letters = ('A'..'Z').to_a.sample(8) + %w[A E I O U].sample(2)
   end
 
   def score
     @user_answer = params[:score].upcase
-    @convert_user_to_array = @user_answer.chars
+    @converted_array = @user_answer.chars
     @letters = params[:letters]
-    @result = @convert_user_to_array.all? { |letter| @letters.include?(letter) }
+    @match_data = @converted_array.all? { |letter| @letters.include?(letter) }
     # raise
+
+    return unless @match_data
+
+    response = URI.open("https://wagon-dictionary.herokuapp.com/#{@user_answer}")
+    json = JSON.parse(response.read)
+    @english_word = json['found']
   end
 end
